1f51d711d1ff265379a8dfd40c8e6f15d857b8a1
Mesele Shishay <messeleshishaymm@gmail.com>
2024-10-12 11:01:24 -0300

    Login Using Username Added

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
@@ -39,9 +40,15 @@ class LoginRequest extends FormRequest
      */
     public function authenticate(): void
     {
+        
         $this->ensureIsNotRateLimited();
-
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! (Auth::attempt([
+            "email" => $this->email,
+            "password" => $this->password
+        ], $this->boolean('remember')) || Auth::attempt([
+            "username" => $this->email,
+            "password" => $this->password
+        ], $this->boolean('remember')))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
@@ -80,6 +87,6 @@ class LoginRequest extends FormRequest
      */
     public function throttleKey(): string
     {
-        return Str::transliterate(Str::lower($this->string('email')).'|'.$this->ip());
+        return Str::transliterate(Str::lower($this->string('email')) . '|' . $this->ip());
     }
 }
